7926379acb08660c27a5fb6252cdb964f30c809d
Enrico <Enrico@LAPTOP-CGURTD77>
2022-11-20 16:06:36 +0100

    [E.C.] - SpringBoot App

---
+++ src/test/java/it/ectn/service/be/servicebe/ServiceBeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ectn.service.be.servicebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
